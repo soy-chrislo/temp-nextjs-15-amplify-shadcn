eacd5959ebc00e6519692e1baee405d37778d8d9
soy-chrislo <green13-16l@hotmail.com>
2025-06-05 15:18:41 -0500

    a

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import auth  from './auth/resource';
-import storage from './storage/resource';
 
 const backend = defineBackend({
   auth: auth,
-  storage: storage
 });
